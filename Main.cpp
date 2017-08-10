75c07a1ba580e3db24562c9c8e82a076be6e032a
Daqing Yi <dqyi11@gmail.com>
2017-08-10 11:47:59 -0700

    with plane and cubes

--- Main.cpp
+++ Main.cpp
@@ -18,6 +18,8 @@ const double delta_stiffness = 10;
 const double default_damping = 5.0;
 const double delta_damping = 1.0;
 
+const double default_cube_mass = 1.0;
+
 using namespace dart::dynamics;
 using namespace dart::simulation;
 
@@ -42,28 +44,71 @@ void setGeometry(const BodyNodePtr& bn)
   bn->setLocalCOM(center);
 }
 
+SkeletonPtr createCube(const Eigen::Vector3d& _position,
+                       const Eigen::Vector3d& _size,
+                       double _mass)
+{
+  dart::dynamics::SkeletonPtr newCubeSkeleton =
+      dart::dynamics::Skeleton::create();
+
+  dart::dynamics::BodyNode::Properties body;
+  body.mName = "cube_link";
+  body.mInertia.setMass(_mass);
+  body.mInertia.setMoment(
+      dart::dynamics::BoxShape::computeInertia(_size, _mass));
+  dart::dynamics::ShapePtr newBoxShape(new dart::dynamics::BoxShape(_size));
+
+  dart::dynamics::FreeJoint::Properties joint;
+  joint.mName = "cube_joint";
+  joint.mT_ParentBodyToJoint = Eigen::Translation3d(_position);
+
+  auto pair
+      = newCubeSkeleton->createJointAndBodyNodePair<dart::dynamics::FreeJoint>(
+        nullptr, joint, body);
+  auto shapeNode = pair.second->createShapeNodeWith<
+      dart::dynamics::VisualAspect,
+      dart::dynamics::CollisionAspect,
+      dart::dynamics::DynamicsAspect>(newBoxShape);
+  shapeNode->getVisualAspect()->setColor(dart::math::randomVector<3>(0.0, 1.0));
+
+  return newCubeSkeleton;
+}
+
 BodyNode* makeRootBody(const SkeletonPtr& di, const std::string& name)
 {
-  BallJoint::Properties properties;
-  properties.mName = name + "_joint";
-  properties.mRestPositions = Eigen::Vector3d::Constant(default_rest_position);
-  properties.mSpringStiffnesses = Eigen::Vector3d::Constant(default_stiffness);
-  properties.mDampingCoefficients = Eigen::Vector3d::Constant(default_damping);
+    // Set up the properties for the Joint
+    RevoluteJoint::Properties properties;
+    properties.mName = name + "_joint";
+    properties.mAxis = Eigen::Vector3d::UnitY();
+    properties.mT_ParentBodyToJoint.translation() =
+        Eigen::Vector3d(0, 0, default_height);
+    properties.mDampingCoefficients[0] = default_damping;
+    properties.mRestPositions[0] = default_rest_position;
+    properties.mSpringStiffnesses[0] = default_stiffness;
 
-  BodyNodePtr bn = di->createJointAndBodyNodePair<BallJoint>(
-        nullptr, properties, BodyNode::AspectProperties(name)).second;
+    // Create a new BodyNode, attached to its parent by a RevoluteJoint
+    BodyNodePtr bn = di->createJointAndBodyNodePair<RevoluteJoint>(
+          nullptr, properties, BodyNode::AspectProperties(name)).second;
 
-  // Make a shape for the Joint
-  const double& R = default_width;
-  std::shared_ptr<EllipsoidShape> ball(
-        new EllipsoidShape(sqrt(2) * Eigen::Vector3d(R, R, R)));
-  auto shapeNode = bn->createShapeNodeWith<VisualAspect>(ball);
-  shapeNode->getVisualAspect()->setColor(dart::Color::Blue());
+    // Make a shape for the Joint
+    const double R = default_width / 2.0;
+    const double h = default_depth;
+    std::shared_ptr<CylinderShape> cyl(new CylinderShape(R, h));
 
-  // Set the geometry of the Body
-  setGeometry(bn);
+    // Line up the cylinder with the Joint axis
+    Eigen::Isometry3d tf(Eigen::Isometry3d::Identity());
+    tf.linear() = dart::math::eulerXYZToMatrix(Eigen::Vector3d(0.0 * M_PI / 180.0,
+                                                               90.0 * M_PI / 180.0,
+                                                               0.0 * M_PI / 180.0));
 
-  return bn;
+    auto shapeNode = bn->createShapeNodeWith<VisualAspect>(cyl);
+    shapeNode->getVisualAspect()->setColor(dart::Color::Blue());
+    shapeNode->setRelativeTransform(tf);
+
+    // Set the geometry of the Body
+    setGeometry(bn);
+
+    return bn;
 }
 
 BodyNode* addBody(const SkeletonPtr& di, BodyNode* parent,
@@ -90,8 +135,9 @@ BodyNode* addBody(const SkeletonPtr& di, BodyNode* parent,
 
   // Line up the cylinder with the Joint axis
   Eigen::Isometry3d tf(Eigen::Isometry3d::Identity());
-  tf.linear() = dart::math::eulerXYZToMatrix(
-        Eigen::Vector3d(90.0 * M_PI / 180.0, 0, 0));
+  tf.linear() = dart::math::eulerXYZToMatrix(Eigen::Vector3d(0.0 * M_PI / 180.0,
+                                                             90.0 * M_PI / 180.0,
+                                                             0.0 * M_PI / 180.0));
 
   auto shapeNode = bn->createShapeNodeWith<VisualAspect>(cyl);
   shapeNode->getVisualAspect()->setColor(dart::Color::Blue());
@@ -118,17 +164,36 @@ int main(int argc, char* argv[])
   BodyNode* bn = makeRootBody(di, "body1");
   bn = addBody(di, bn, "body2");
   bn = addBody(di, bn, "body3");
-  bn = addBody(di, bn, "body4");
+  //bn = addBody(di, bn, "body4");
 
   // Set the initial position of the first DegreeOfFreedom so that the di
   // starts to swing right away
-  di->getDof(1)->setPosition(120 * M_PI / 180.0);
-  di->getDof(2)->setPosition(90 * M_PI / 180.0);
-  di->getDof(3)->setPosition(90 * M_PI / 180.0);
-  di->getDof(4)->setPosition(90 * M_PI / 180.0);
-  di->getDof(5)->setPosition(270 * M_PI / 180.0);
+  di->getDof(0)->setPosition(30 * M_PI / 180.0);
+  //di->getDof(1)->setPosition(90 * M_PI / 180.0);
+  //di->getDof(2)->setPosition(270 * M_PI / 180.0);
+  //di->getDof(3)->setPosition(90 * M_PI / 180.0);
+
+  Eigen::Vector3d planePos;
+  Eigen::Vector3d planeSize;
+  planeSize << 8.0, 8.0, 0.02;
+  SkeletonPtr plane = createCube(planePos, planeSize, default_cube_mass);
+
+  Eigen::Vector3d cube1Pos;
+  Eigen::Vector3d cube1Size;
+  cube1Pos << 1.0, 1.0, 0.25;
+  cube1Size << 0.5, 0.5, 0.5;
+  SkeletonPtr cube1 = createCube(cube1Pos, cube1Size, default_cube_mass);
+
+  Eigen::Vector3d cube2Pos;
+  Eigen::Vector3d cube2Size;
+  cube2Pos << 2.0, 1.0, 0.25;
+  cube2Size << 0.5, 0.5, 0.5;
+  SkeletonPtr cube2 = createCube(cube2Pos, cube2Size, default_cube_mass);
 
   world->addSkeleton(di);
+  world->addSkeleton(plane);
+  world->addSkeleton(cube1);
+  world->addSkeleton(cube2);
 
   // create a window and link it to the world
   MyWindow window;
